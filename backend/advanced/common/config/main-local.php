5e48cf8964283b845614bd396504779014e8caee
Milan Limbasiya <milan.limbasiya.techmitraa@gmail.com>
2019-02-15 18:31:20 +0530

    Backend Updated

--- backend/advanced/common/config/main-local.php
+++ backend/advanced/common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=ticket_market_place',
             'username' => 'root',
-            'password' => '',
+            'password' => 'techmitraa@123',
             'charset' => 'utf8',
         ],
         'mailer' => [
